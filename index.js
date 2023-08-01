34e0088aea98fbb925574e96e9a2df5754306ab0
sagarbendale2004 <sagarbendale2004@gmail.com>
2023-08-01 18:39:23 +0530

    Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function myfun() {
+    var navbar = document.getElementById("navbar");
+    var info = document.getElementById("info");
+
+    if (screen.width > 1400) {
+        navbar.style.paddingLeft = "60px 80px"
+    }
+}
+
+myfun();
